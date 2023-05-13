101ddfac6dcc2f27045b8bfe71be727041b4109a
Umuhasna <umuhasna123@gmail.com>
2023-05-13 22:22:21 +0300

    Build a linked list for the PATH directories

---
+++ add_nodetoken_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listtoken_t list
+ * @head: points to a pointer that points to the first node
+ * @token: the token to add
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+listtoken_t *add_nodetoken_end(listtoken_t **head, char *token)
+{
+	listtoken_t *new_node, *last_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listtoken_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->token = token;
+	new_node->token_length = strlen(token);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+
+		last_node->next = new_node;
+	}
+
+	return (last_node);
+}
