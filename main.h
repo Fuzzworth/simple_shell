c193662bfbf15007b4d4ceda65b3c326728a4aa0
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-05-13 20:33:04 +0200

    Added _getenv.c function

--- main.h
+++ main.h
@@ -13,4 +13,5 @@ char **array_maker(char *input, char *delimiter);
 int _fork(char **array_of_tokens);
 char *_which(char *filename);
 int _print_env(void);
+char *_getenv(const char *name);
 #endif
